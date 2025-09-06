0a14c836b15c956e6ad9b5ccc4c502c825cacd11
yujf <296519653@qq.com>
2025-09-06 20:26:23 +0800

    init

---
+++ src/utils/tauri.js
@@ -0,0 +1,9 @@
+import * as api from '@tauri-apps/api';
+import * as fs from '@tauri-apps/plugin-fs';
+import * as dialog from '@tauri-apps/plugin-dialog';
+
+export default {
+  fs,
+  dialog,
+  api,
+};
