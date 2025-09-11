a11ac2cfda51a12d8ef93007c94d5c14e3012a74
余金锋 <296519653@qq.com>
2025-09-11 08:48:31 +0800

    sas

--- script/rename.js
+++ script/rename.js
@@ -1,14 +1,10 @@
-import { execSync } from 'child_process';
-import fs from 'fs';
+import fs from 'node:fs/promises';
 
 const ext = process.platform === 'win32' ? '.exe' : '';
 
-const rustInfo = execSync('rustc -vV');
-const targetTriple = /host: (\S+)/g.exec(rustInfo)[1];
-if (!targetTriple) {
-    console.error('Failed to determine platform target triple');
-}
-fs.renameSync(
-    `app${ext}`,
-    `./src-tauri/binaries/app-${targetTriple}${ext}`
-);
+const rust_info = await Bun.$`rustc -vV`.text();
+const target_triple = /host: (\S+)/g.exec(rust_info)[1];
+if (!target_triple) console.error('Failed to determine platform target triple');
+const target_dir = './src-tauri/binaries';
+await fs.mkdir(target_dir, { recursive: true, force: true });
+await fs.rename(`app${ext}`, `${target_dir}/app-${target_triple}${ext}`);
